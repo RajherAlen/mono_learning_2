d2e93632e4707e1418819b2fcd520577ae3e3274
Alen Rajher <alen.rajher@gmail.com>
2021-05-24 19:22:18 +0200

    Extract CSS & Minify HTML/CSS/JS

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,25 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
 	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// 3 - Creates `style` nodes from JS strings
+					"style-loader",
+					// 2 - Translates CSS into CommonJS
+					"css-loader",
+					// 1 - Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		],
+	},
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist"),
@@ -13,4 +29,9 @@ module.exports = merge(common, {
 	devServer: {
 		contentBase: "./dist",
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+		}),
+	],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,10 @@ const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
 	mode: "production",
@@ -9,5 +13,34 @@ module.exports = merge(common, {
 		filename: "main.[contenthash].js",
 		path: path.resolve(__dirname, "dist"),
 	},
-	plugins: [new CleanWebpackPlugin()],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// 3. extract css into files
+					MiniCssExtractPlugin.loader,
+					// 2 - Translates CSS into CommonJS
+					"css-loader",
+					// 1 - Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		],
+	},
+	optimization: {
+		minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: "[main.[contenthash].css" }),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+			minify: {
+				removeAttributeQuotes: true,
+				collapseWhitespace: true,
+				removeComments: true;
+			}
+		}),
+	],
 });
